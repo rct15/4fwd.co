abdb6e4a40c42e1a727dc9b2c45e45194548f9e8
rct15 <54568595+rct15@users.noreply.github.com>
2024-10-26 22:13:54 -0300

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,9 @@
 const config = {
-  result_page: false, // After get the value from KV, if use a page to show the result.
-  theme: "", // Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
+  result_page: true, // After get the value from KV, if use a page to show the result.
+  theme: "theme/urlcool", // Homepage theme, use the empty value for default theme. To use urlcool theme, please fill with "theme/urlcool" .
   cors: true, // Allow Cross-origin resource sharing for API requests.
   unique_link: false, // If it is true, the same long url will be shorten into the same short url
-  custom_link: true, // Allow users to customize the short url.
+  custom_link: false, // Allow users to customize the short url.
   overwrite_kv: false, // Allow user to overwrite an existed key.
   snapchat_mode: false, // The link will be distroyed after access.
   visit_count: false, // Count visit times.
