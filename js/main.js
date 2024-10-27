a4566366db0ec25b0a41fd6398cc73fd8e69f6bb
rct15 <54568595+rct15@users.noreply.github.com>
2024-10-26 23:41:53 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -389,7 +389,7 @@ function buildValueTxt(longUrl) {
   return valueTxt
 }
 
-addEventListener("fetch", async event => {
+addEventListener('DOMContentLoaded', function() {
   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
   var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
