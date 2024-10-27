e9eabe067985e9a15206c5f1473e12c73d5a7a50
rct15 <54568595+rct15@users.noreply.github.com>
2024-10-26 21:14:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -388,12 +388,12 @@ function buildValueTxt(longUrl) {
   valueTxt.innerText = longUrl
   return valueTxt
 }
-
+/*
 document.addEventListener('DOMContentLoaded', function() {
   var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
   var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
     return new bootstrap.Popover(popoverTriggerEl)
   });
 
-  loadUrlList();
+  loadUrlList();*/
 });
